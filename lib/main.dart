a133c8819acde0b05b36c6ebb82e6ac55e84aa6d
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-05 22:28:27 +0800

    change to Splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dsmap/screens/home.dart';
+import 'package:dsmap/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const Home(),
+      home: const Splash(),
     );
   }
 }
