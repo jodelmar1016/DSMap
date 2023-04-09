92abdadaaff0495ec5726ac7d7c5cc0de0ce685f
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-09 20:41:58 +0800

    temporary

--- lib/services/dataService.dart
+++ lib/services/dataService.dart
@@ -5,6 +5,33 @@ final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _collection = _firestore.collection('reports');
 
 class DataService {
+  // ADD REPORT
+  static Future<Response> addReport({
+    required String barangay,
+    required String message,
+    required DateTime date,
+  }) async {
+    Response response = Response();
+    DocumentReference documentReferencer = _collection.doc();
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "employee_name": barangay,
+      "position": message,
+      "contact_no": date
+    };
+
+    var result = await documentReferencer.set(data).whenComplete(() {
+      response.code = 200;
+      response.message = "Sucessfully added to the database";
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
+    return response;
+  }
+
+  // GET ALL REPORTS
   static Stream<QuerySnapshot> readReports() {
     CollectionReference notesItemCollection = _collection;
 
