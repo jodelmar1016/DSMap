6fc961f27eec35098f87203d36c4387b96a72019
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-20 13:35:57 +0800

    handle login

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dsmap/screens/user/home.dart';
 import 'package:dsmap/authentication/register.dart';
+import 'package:dsmap/services/auth.dart';
+import 'package:dsmap/models/response.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -10,13 +12,37 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool passwordObscured = true;
+  bool _isLoading = false;
+
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final AuthService _auth = AuthService();
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // EMAIL
     Widget email = Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40),
       child: TextFormField(
+        controller: _email,
         keyboardType: TextInputType.emailAddress,
+        validator: (value) {
+          if (value != null) {
+            if (value.contains('@') && value.endsWith('.com')) {
+              return null;
+            }
+            return 'Enter a Valid Email Address';
+          }
+        },
         decoration: InputDecoration(
           border: OutlineInputBorder(),
           prefixIcon: Icon(Icons.email),
@@ -30,11 +56,31 @@ class _LoginState extends State<Login> {
     Widget password = Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40),
       child: TextFormField(
-        obscureText: true,
+        controller: _password,
+        obscureText: passwordObscured,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty) {
+            return 'This field is required';
+          }
+          if (value.trim().length < 8) {
+            return 'Password must be at least 8 characters in length';
+          }
+          return null;
+        },
         decoration: InputDecoration(
           border: OutlineInputBorder(),
           prefixIcon: Icon(Icons.lock),
           labelText: 'Password',
+          suffixIcon: IconButton(
+            onPressed: () {
+              setState(() {
+                passwordObscured = !passwordObscured;
+              });
+            },
+            icon: Icon(
+              passwordObscured ? Icons.visibility_off : Icons.visibility,
+            ),
+          ),
           // contentPadding: EdgeInsets.fromLTRB(10, 10, 10, 10),
         ),
       ),
@@ -45,11 +91,34 @@ class _LoginState extends State<Login> {
       child: SizedBox(
         width: double.infinity,
         child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => Home()),
-            );
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              setState(() {
+                _isLoading = true;
+              });
+              Response result = await _auth.signInEmailPassword(
+                  _email.text.trim(), _password.text.trim());
+              setState(() {
+                _isLoading = false;
+              });
+              if (result.code == 500) {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text(result.message.toString()),
+                    );
+                  },
+                );
+              } else {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                );
+              }
+            }
           },
           child: Text(
             'Login',
@@ -60,41 +129,44 @@ class _LoginState extends State<Login> {
     );
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: GestureDetector(
         onTap: () {
           FocusManager.instance.primaryFocus?.unfocus();
         },
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 100),
-              child: Column(
-                children: [
-                  Image.asset(
-                    'assets/logo.png',
-                    width: 250,
-                  ),
-                  Text(
-                    'DSMAP',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.green,
+        child: Stack(
+          children: [
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 100),
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/logo.png',
+                      width: 250,
                     ),
-                  ),
-                  SizedBox(height: 70),
-                  email,
-                  SizedBox(height: 20),
-                  password,
-                  SizedBox(height: 60),
-                  loginButton,
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 40),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Don\'t have an account?'),
-                        TextButton(
+                    Text(
+                      'DSMAP',
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.green,
+                      ),
+                    ),
+                    SizedBox(height: 70),
+                    email,
+                    SizedBox(height: 20),
+                    password,
+                    SizedBox(height: 60),
+                    loginButton,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Don\'t have an account?'),
+                          TextButton(
                             onPressed: () {
                               Navigator.pushReplacement(
                                 context,
@@ -105,14 +177,26 @@ class _LoginState extends State<Login> {
                             },
                             child: Text(
                               'Register new',
-                            ))
-                      ],
-                    ),
-                  )
-                ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
+            if (_isLoading)
+              Container(
+                color: Colors.black.withOpacity(0.5),
+                child: Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.grey[200],
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                  ),
+                ),
+              ),
+          ],
         ),
       ),
     );
