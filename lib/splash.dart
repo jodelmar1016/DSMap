16de6e26efd7e73915a53803df87283e789c8276
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-05-15 18:32:22 +0800

    navigate to Login

--- lib/splash.dart
+++ lib/splash.dart
@@ -21,7 +21,7 @@ class _SplashState extends State<Splash> {
     await Future.delayed(
       Duration(milliseconds: 1500),
       () {
-        Navigator.pushReplacement(context, MyPageRouter(page: Home()));
+        Navigator.pushReplacement(context, MyPageRouter(page: Login()));
       },
     );
   }
