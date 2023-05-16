66d64ce45a73e9a68886ace20cc497dd67814e39
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-05-16 18:31:06 +0800

    display markers

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,34 +1,62 @@
+import 'package:dsmap/services/dataService.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Mapping extends StatelessWidget {
+class Mapping extends StatefulWidget {
   const Mapping({super.key});
+
+  @override
+  State<Mapping> createState() => _MappingState();
+}
+
+class _MappingState extends State<Mapping> {
+  Set<Marker> _markers = Set();
+  bool _isLoading = true;
+
+  void getMarkers() async {
+    Set<Marker> temp = await DataService.getAllReports();
+    setState(() {
+      _isLoading = false;
+      _markers = temp;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMarkers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Mapping'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Mapping',
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: Text('Mapping'),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          if (_isLoading == false)
+            GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(17.771387, 121.527988),
+                zoom: 14,
+              ),
+              markers: _markers,
+            ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.grey[200],
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
                 ),
               ),
-              SizedBox(height: 10),
-              Image.asset(
-                'assets/map.jpg',
-                width: 400,
-              ),
-            ],
-          ),
-        ));
+            ),
+        ],
+      ),
+    );
   }
 }
